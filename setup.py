ecd461e9ce2623cdadf2eb2ec78fcace681320ce
hsn2 <912372447@qq.com>
2022-12-03 13:25:42 +0800

    change

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fraction',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/hsn8086/Fraction',
+    license='MIT',
+    author='hsn8086',
+    author_email='hsn1919810@gmail.com',
+    description='A fraction module, designed to preserve the maximum precision of numbers and the quick calculation of the simplest fractions of decimals.'
+)
