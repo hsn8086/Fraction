a31d6e59526c5d026d18b5340ca5c843b8535ae1
hsn2 <912372447@qq.com>
2022-12-03 10:55:41 +0800

    add: method: ['__add__', '__str__']

--- fraction.py
+++ fraction.py
@@ -12,18 +12,36 @@ class Fraction:
             self.denominator = 1
             self.numerator = num
         elif type(num) == float:
-            # 取标准化进度值
-            precision_normal = pow(10, precision)
-            precision_int = int(precision_normal)
-
-            # 取未化简分子
-            num_int = int(num * precision_int)
-
-            # 取最大公约数
-            hcf = math.gcd(num_int, precision_int)
+            # 取标准化精度值
+            precision_int = int(10 ** precision)
 
             # 写入
-            self.denominator = int(precision_int / hcf)
-            self.numerator = int(num_int / hcf)
+            self.denominator = precision_int
+
+            # 取未化简分子
+            self.numerator = int(num * precision_int)
+
+            # 化简
+            self.simplify()
             # 注:分母为1*precision_int
+
+    def simplify(self):
+        hcf = math.gcd(self.denominator, self.numerator)
+        if hcf:
+            self.numerator = int(self.numerator / hcf)
+            self.denominator = int(self.denominator / hcf)
+
+    def __add__(self, other):
+        if type(other) == Fraction:
+            other: Fraction
+            rt = Fraction(1)
+            rt.numerator = self.numerator * other.denominator + self.denominator * other.numerator
+            rt.denominator = self.denominator * other.denominator
+            rt.simplify()
+            return rt
+
+    def __str__(self):
+        return f'{self.numerator}/{self.denominator}'
+
+
 
