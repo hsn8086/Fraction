e04b3c1ec80cceaa2a3eb97476f0fac77dec9df4
hsn2 <912372447@qq.com>
2022-12-03 14:04:40 +0800

    change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fraction',
-    version='1.0.0',
+    version='1.0.1',
     packages=[''],
     url='https://github.com/hsn8086/Fraction',
     license='MIT',
