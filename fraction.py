cb55d05cf6b863dec2f57c0f74c4c1ac523ad6ec
hsn2 <912372447@qq.com>
2022-12-03 12:06:32 +0800

    add: lots of method

--- fraction.py
+++ fraction.py
@@ -99,6 +99,29 @@ class Fraction:
         rt.numerator = self.numerator ** power
         return rt % modulo if modulo else rt
 
+    '''def __and__(self, other):
+        return self & other'''
+
+    # compare
+    def __lt__(self, other):
+        return float(self) < float(other)
+
+    def __le__(self, other):
+        return float(self) <= float(other)
+
+    def __gt__(self, other):
+        return float(self) > float(other)
+
+    def __ge__(self, other):
+        return float(self) >= float(other)
+
+    def __eq__(self, other):
+        return float(self) == float(other)
+
+    def __ne__(self, other):
+        return not self == other
+
+    # transform
     def __float__(self):
         return self.numerator / self.denominator
 
@@ -111,4 +134,4 @@ class Fraction:
             return '0'
 
 
-
+print(Fraction(3) != 2)
